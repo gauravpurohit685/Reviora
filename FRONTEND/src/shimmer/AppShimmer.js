cdaf2283a229a862b483e61e7464c6a2ab5a0a08
Gaurav Purohit <gauravpurohit685@gmail.com>
2026-09-06 22:11:01 +0530

    feat: loader size increase

--- FRONTEND/src/shimmer/AppShimmer.js
+++ FRONTEND/src/shimmer/AppShimmer.js
@@ -3,7 +3,7 @@
 const AppShimmer = () => {
     return (
         <div class="flex min-h-screen items-center justify-center">
-            <span class="loading loading-spinner loading-lg"></span>
+            <span class="loading loading-spinner loading-xl"></span>
         </div>
     )
 }
